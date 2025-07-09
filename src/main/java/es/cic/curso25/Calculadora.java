89f769df540ff7aedab1de6a3fd2aaee85764cfa
AlvaroG <aayestarain@formacion.cic.es>
2025-07-09 12:37:49 +0200

    Funcionalidad de sumar añadida.

--- src/main/java/es/cic/curso25/Calculadora.java
+++ src/main/java/es/cic/curso25/Calculadora.java
@@ -3,14 +3,12 @@ package es.cic.curso25;
 public class Calculadora {
     // Ante la duda, privado. Si no se va a usar, privado.
     private double total;
+
     public double getTotal() {
         return total;
     }
 
-    public void setTotal (double total){
-        this.total = total;
-    }
-    public void noHagoNada(){
-        total = 3;
+    public void sumar(double valor) {
+        total += valor;
     }
 }
--- src/test/java/es/cic/curso25/CalculadoraTest.java
+++ src/test/java/es/cic/curso25/CalculadoraTest.java
@@ -1,18 +1,27 @@
 package es.cic.curso25;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 /* Zona de importaciones. Se importa toda esa linea acabada en "Test" para que el programa sepa a donde está apuntando */
 
 public class CalculadoraTest {
     @Test
-    public void testLanzar() {
-        Calculadora c = new Calculadora();
-        Calculadora d = c;
-        c.setTotal(3);
-        //d apunta AL MISMO SITIO QUE C. si C.total es 3, D.total TAMBIÉN es 3
-        c = new Calculadora();
-        c.setTotal(4.5);
-        
+    public void testSumar() {
+        Calculadora cut = new Calculadora();
+        //cut = ClassUnderTest
+
+        cut.sumar(5.6);
+
+        double valorActual = cut.getTotal();
+        assertEquals(5.6,valorActual,0.001); 
+        //Delta es la variacion que le permitimos porque existen decimales muy largos y pueden tener pequeñas diferencias
+
+        cut.sumar(3.4);
+        valorActual = cut.getTotal();
+
+        assertEquals(9, valorActual,0.000001);
     } 
     
 }
+
