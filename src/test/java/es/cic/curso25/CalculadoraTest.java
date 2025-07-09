3be20bbfa7ec62576b364df38113645934633949
AlvaroG <aayestarain@formacion.cic.es>
2025-07-09 11:46:24 +0200

    Sin funcionalidad. Añadidas pruebas simples.

---
+++ src/test/java/es/cic/curso25/CalculadoraTest.java
@@ -0,0 +1,18 @@
+package es.cic.curso25;
+
+import org.junit.jupiter.api.Test;
+/* Zona de importaciones. Se importa toda esa linea acabada en "Test" para que el programa sepa a donde está apuntando */
+
+public class CalculadoraTest {
+    @Test
+    public void testLanzar() {
+        Calculadora c = new Calculadora();
+        Calculadora d = c;
+        c.total = 3;
+        //d apunta AL MISMO SITIO QUE C. si C.total es 3, D.total TAMBIÉN es 3
+        c = new Calculadora();
+        c.total = 4.5;
+        
+    } 
+    
+}
