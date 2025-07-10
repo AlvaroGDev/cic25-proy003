ee927dd94b3aee4ce1aa49bf5ccc46e596d11df5
AlvaroG <aayestarain@formacion.cic.es>
2025-07-10 09:59:14 +0200

    Añadida funcionalidad excepción de divisón por cero

--- src/main/java/es/cic/curso25/Calculadora.java
+++ src/main/java/es/cic/curso25/Calculadora.java
@@ -1,5 +1,6 @@
 package es.cic.curso25;
 
+// Aquí habría un main
 public class Calculadora {
     // Ante la duda, privado. Si no se va a usar, privado.
     private double total;
@@ -12,17 +13,22 @@ public class Calculadora {
         total += valor;
     }
 
-    public void restar(double valor){
-        total-=valor;
+    public void restar(double valor) {
+        total -= valor;
     }
 
-    public void multiplicar(double valor){
-        total*=valor;
+    public void multiplicar(double valor) {
+        total *= valor;
     }
 
-    public void dividir(double valor){
-        if(valor == 0)
-            throw new Error("No dividas por 0");
-        else total/=valor;
+    public void dividir(double valor) {
+        if (valor == 0)
+            throw new ArithmeticException("División por cero");
+        else
+            total /= valor;
+        /*
+         * En este caso, si testeamos, dividimos por 0 y da error por la
+         * arithmeticexception estaría bien porque es lo que hemos querido
+         */
     }
 }
--- src/test/java/es/cic/curso25/CalculadoraTest.java
+++ src/test/java/es/cic/curso25/CalculadoraTest.java
@@ -1,6 +1,7 @@
 package es.cic.curso25;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 /* Zona de importaciones. Se importa toda esa linea acabada en "Test" para que el programa sepa a donde está apuntando */
@@ -35,7 +36,7 @@ public class CalculadoraTest {
         // El valor pasa a ser -6
         valorActual = cut.getTotal();
         // valorActual debería ser -6. Usamos el get porque es privado
-        assertEquals(-6, valorActual);
+        assertEquals(-6, valorActual, 0.000001);
     }
 
     @Test
@@ -44,20 +45,26 @@ public class CalculadoraTest {
         cut.sumar(5);
         cut.multiplicar(5);
         // Sumamos primero 5 para que no multiplique por 0 y poder revisar que funciona
-        // correctamente
+        
         valorActual = cut.getTotal();
         // Recogemos el total como siempre
 
         assertEquals(25, valorActual);
         // Por último comparamos
     }
-
+    
     @Test
     public void testDividir() {
 
         cut.sumar(10);
         cut.dividir(5);
+
         valorActual = cut.getTotal();
+
+      assertThrows(ArithmeticException.class, 
+      ()->cut.dividir(0));
+        // Tenemos que usar esa sintaxis para poder 
+
         assertEquals(2, valorActual);
     }
 }
